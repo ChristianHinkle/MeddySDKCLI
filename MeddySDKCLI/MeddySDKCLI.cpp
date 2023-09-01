7714f9e1fe3ed8890a7fcefb66451e61fe925b6f
Christian Hinkle <christianhinko@gmail.com>
2023-08-31 21:14:43 -0400

    Create default CMake project with Visual Studio.
    Created with "Place solution and project in the same directory" true.
    
    Make gitignore for this project.

---
+++ MeddySDKCLI/MeddySDKCLI.cpp
@@ -0,0 +1,12 @@
+﻿// MeddySDKCLI.cpp : Defines the entry point for the application.
+//
+
+#include "MeddySDKCLI.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
