00fb39c06a747478c1c8dc051e77a296019c1123
Christian Hinkle <christianhinko@gmail.com>
2024-02-04 06:25:09 -0500

    Call Meddyproject experiment function for Boost in main.

--- MeddySDKCLI/Source/Private/MeddySDKCLI.cpp
+++ MeddySDKCLI/Source/Private/MeddySDKCLI.cpp
@@ -2,10 +2,13 @@
 #include "MeddySDKCLI.h"
 
 #include <iostream>
+#include "MeddySDK_Meddyproject.h"
 
 int main(int argc, char** argv)
 {
     std::cout << "Hello CMake." << std::endl;
+
+    MeddySDK::Meddyproject::MyBoostFilesystemExperiment();
 
     return 0;
 }
