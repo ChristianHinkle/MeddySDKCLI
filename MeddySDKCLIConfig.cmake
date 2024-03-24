90c7ab7b0f5b130bed0a6fdb61538bc5d4b99ce5
Christian Hinkle <christianhinko@gmail.com>
2024-03-24 06:28:47 -0400

    [CMake] Update documentation for new alias target convention.
    Set up aliases in the export's config file, with documentation.

--- MeddySDKCLIConfig.cmake
+++ MeddySDKCLIConfig.cmake
@@ -6,3 +6,12 @@ find_dependency(Meddyproject 0 CONFIG)
 
 # Include our export. This imports all of our targets.
 include("${CMAKE_CURRENT_LIST_DIR}/MeddySDKCLIExport.cmake")
+
+#
+# Add alias targets whose names match those from the project file.
+#
+# It's important to make sure the names are the same for consistency. Namely, so that build
+# interface users can use `OVERRIDE_FIND_PACKAGE` with `FetchContent_Declare()`.
+#
+
+add_executable(::MeddySDKCLI ALIAS MeddySDKCLI)
