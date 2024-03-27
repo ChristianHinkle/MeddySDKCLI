d979aa5c43f7f185bcef2b0f3700ad04a46f6264
Christian Hinkle <christianhinko@gmail.com>
2024-03-27 03:15:58 -0400

    [Main] Rename file with program entry point to Main.

--- Source/Private/Main.cpp
+++ Source/Private/Main.cpp
@@ -1,14 +1,12 @@
 // Copyright (c) 2023-2024 Christian Hinkle, Brian Hinkle.
 
-#include "MeddySDKCLI.h"
+#include "Main.h"
 
 #include <iostream>
 #include "MeddySDK_Meddyproject.h"
 
 int main(int argc, char** argv)
 {
-    std::cout << "Hello CMake." << std::endl;
-
     MeddySDK::Meddyproject::MyBoostFilesystemExperiment();
 
     return 0;
