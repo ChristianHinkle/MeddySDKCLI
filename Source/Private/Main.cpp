f62d782e1ba1a21d5616d4b887c347e9f2141828
Christian Hinkle <christianhinko@gmail.com>
2025-08-04 19:59:28 -0600

    [Source] [Commands] Make some basic commands (--help) as placeholder.

--- Source/Private/Main.cpp
+++ Source/Private/Main.cpp
@@ -1,6 +1,34 @@
 // Copyright (c) 2023-2025 Christian Hinkle, Brian Hinkle.
 
+#include <iostream>
+#include <string_view>
+
 int main(int argc, char** argv)
 {
+    if (argc <= 1)
+    {
+        std::cout << "usage: meddy [--help]" << '\n';
+        std::cout.flush();
+        return 0;
+    }
+
+    if (argc == 2)
+    {
+        const std::string_view arg = argv[1];
+
+        if (arg == "--help")
+        {
+            std::cout << "There is nothing so far." << '\n';
+            std::cout.flush();
+            return 0;
+        }
+
+        std::cout << "meddy: '" << arg << "' is not a meddy command. See 'meddy --help'." << '\n';
+        std::cout.flush();
+        return 0;
+    }
+
+    std::cout << "meddy: What is even that?" << '\n';
+    std::cout.flush();
     return 0;
 }
