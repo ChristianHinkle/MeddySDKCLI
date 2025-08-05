0acf4b862f83d8bcf204ae041646cba89bb68589
Christian Hinkle <christianhinko@gmail.com>
2025-08-04 19:31:40 -0600

    [Tests] [Code] Delete placeholder main function code, which has now been moved to a proper test in the MeddySDK_Meddyproject library. Also, delete "Main.h".

--- Source/Private/Main.cpp
+++ Source/Private/Main.cpp
@@ -2,21 +2,7 @@
 
 #include "Main.h"
 
-#include "Path.h"
-#include <iostream>
-
 int main(int argc, char** argv)
 {
-    std::filesystem::path videosPath =
-        std::filesystem::weakly_canonical("c:/Users/Meddy Man/Videos");
-
-    std::cout << videosPath << std::endl;
-
-    std::filesystem::path videosMeddyprojectPath =
-        MeddySDK::Meddyproject::Path::MakeProjectPath(std::filesystem::path(videosPath));
-
-    std::cout << std::endl;
-    std::cout << videosMeddyprojectPath << std::endl;
-
     return 0;
 }
