73538d2a0eba0ef829a271dedd3b93b9dd7f86be
Christian Hinkle <christianhinko@gmail.com>
2025-10-31 14:05:03 -0600

    [cmake] Separate project-specific naming into centralized variables (prefixed with "MY_BASE_PROJECT_") to allow for easier renaming.
    Side effect: Now, our aliased namespaced target names begin with "::" again. So it is now `::MeddySDKCLI::Source` instead of `MeddySDKCLI::Source`.

--- MeddySDKCLIConfig.cmake
+++ MeddySDKCLIConfig.cmake
@@ -1,12 +1,21 @@
 # Copyright (c) 2023-2025 Christian Hinkle, Brian Hinkle.
 
+# Write out the main project name here, in one place, to make renaming easier. This is mainly used by our
+# boilerplate code. There may be other mentions of the project's name mixed in certain places of this file, so
+# don't rely on these variables alone for renaming.
+SET(MY_BASE_PROJECT_NAME_FULL "MeddySDKCLI")
+string(TOLOWER ${MY_BASE_PROJECT_NAME_FULL} MY_BASE_PROJECT_NAME_FULL_LOWERCASE)
+string(TOUPPER ${MY_BASE_PROJECT_NAME_FULL} MY_BASE_PROJECT_NAME_FULL_UPPERCASE)
+SET(MY_BASE_PROJECT_NAME_NAMESPACE "")
+SET(MY_BASE_PROJECT_NAME_LEAFNAME "MeddySDKCLI")
+
 include(CMakeFindDependencyMacro)
 
 # Get the Meddyproject export.
 find_dependency(Meddyproject 0 CONFIG)
 
 # Include our export. This imports all of our targets.
-include("${CMAKE_CURRENT_LIST_DIR}/MeddySDKCLIExport.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/${MY_BASE_PROJECT_NAME_FULL}Export.cmake")
 
 #
 # Add alias targets whose names match those from the project file.
@@ -15,5 +24,5 @@ include("${CMAKE_CURRENT_LIST_DIR}/MeddySDKCLIExport.cmake")
 # interface users can use `OVERRIDE_FIND_PACKAGE` with `FetchContent_Declare()`.
 #
 
-add_executable(MeddySDKCLI::Source ALIAS MeddySDKCLI_Source)
-add_executable(MeddySDKCLI::Executable ALIAS MeddySDKCLI_Executable)
+add_executable(${MY_BASE_PROJECT_NAME_NAMESPACE}::${MY_BASE_PROJECT_NAME_LEAFNAME}::Source ALIAS ${MY_BASE_PROJECT_NAME_FULL}_Source)
+add_executable(${MY_BASE_PROJECT_NAME_NAMESPACE}::${MY_BASE_PROJECT_NAME_LEAFNAME}::Executable ALIAS ${MY_BASE_PROJECT_NAME_FULL}_Executable)
